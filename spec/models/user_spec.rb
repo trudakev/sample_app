715690eca50f2bf9094d1ed9fd586d68139f66a9
Taras Rudakevych <trudakev@softserveinc.com>
2012-08-31 12:24:33 +0300

    Finish sigh in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -44,7 +45,7 @@ describe User do
     it { should_not be_valid }
   end
 
-describe "when email format is invalid" do
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -110,4 +111,9 @@ describe "when email format is invalid" do
     end
   end
   
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 end
